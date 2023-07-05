7c352d902a3282dff64f3203dcb07d29464e7f59
Space Walker <spacedoesrs@gmail.com>
2023-07-05 13:55:33 +0200

    networking: add more javadoc to CommonChannels

--- libraries/networking/networking-mc13w41a-mc1.13-pre2/src/main/java/net/ornithemc/osl/networking/impl/CommonChannels.java
+++ libraries/networking/networking-mc13w41a-mc1.13-pre2/src/main/java/net/ornithemc/osl/networking/impl/CommonChannels.java
@@ -2,6 +2,10 @@ package net.ornithemc.osl.networking.impl;
 
 /**
  * Common data channels used internally.
+ * 
+ * The channel identifiers follow conventions set by Vanilla custom
+ * payload packets. They consist of a domain name and a channel name,
+ * separated by a {@code |} character.
  */
 public final class CommonChannels {
 
