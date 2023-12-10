8dfca0f77e444e18cc90e506ab07603d2c509e92
Space Walker <spacedoesrs@gmail.com>
2023-12-10 16:04:51 +0100

    remove resource reload events for 1.5- resource loader api impls
    the events were already unused, as the mixins that invoked them were removed
    however, the event relied on the Minecraft#reloadResources method, which did
    not exist in 1.5 - it was added in 13w21a

--- libraries/resource-loader/resource-loader-mc1.3-pre-07261249-mc13w07a/src/main/java/net/ornithemc/osl/resource/loader/api/ResourceLoaderEvents.java
+++ libraries/resource-loader/resource-loader-mc1.3-pre-07261249-mc13w07a/src/main/java/net/ornithemc/osl/resource/loader/api/ResourceLoaderEvents.java
@@ -27,37 +27,4 @@ public class ResourceLoaderEvents {
 	 */
 	public static final Event<Consumer<Consumer<ModTexturePack>>> ADD_DEFAULT_TEXTURE_PACKS = Event.consumer();
 
-	/**
-	 * This event is invoked before resources are reloaded.
-	 * 
-	 * <p>
-	 * Callbacks to this event should be registered in your mod's entrypoint,
-	 * and can be done as follows:
-	 * 
-	 * <pre>
-	 * {@code
-	 * ResourceLoaderEvents.START_RESOURCE_RELOAD.register(() -> {
-	 * 	...
-	 * });
-	 * }
-	 * </pre>
-	 */
-	public static final Event<Runnable> START_RESOURCE_RELOAD = Event.runnable();
-	/**
-	 * This event is invoked after resources are reloaded.
-	 * 
-	 * <p>
-	 * Callbacks to this event should be registered in your mod's entrypoint,
-	 * and can be done as follows:
-	 * 
-	 * <pre>
-	 * {@code
-	 * ResourceLoaderEvents.END_RESOURCE_RELOAD.register(() -> {
-	 * 	...
-	 * });
-	 * }
-	 * </pre>
-	 */
-	public static final Event<Runnable> END_RESOURCE_RELOAD   = Event.runnable();
-
 }
