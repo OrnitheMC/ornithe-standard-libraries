df06bbe9f39e8ec57f6e2ab4668c0bd4119f245f
Space Walker <spacedoesrs@gmail.com>
2024-02-07 20:11:51 +0100

    downgradle loom to 1.3 and loader to 0.14.25 for now
    there's a remapping bug that hits resource loader in several versions that causes a crash in prod

--- libraries/resource-loader/resource-loader-mc13w26a-mc1.12.2/src/main/java/net/ornithemc/osl/resource/loader/impl/mixin/client/TranslationStorageMixin.java
+++ libraries/resource-loader/resource-loader-mc13w26a-mc1.12.2/src/main/java/net/ornithemc/osl/resource/loader/impl/mixin/client/TranslationStorageMixin.java
@@ -4,63 +4,94 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
 
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
-import net.minecraft.client.resource.Resource;
-import net.minecraft.client.resource.language.TranslationStorage;
-import net.minecraft.client.resource.manager.ResourceManager;
-import net.minecraft.resource.Identifier;
 import org.quiltmc.parsers.json.JsonReader;
+
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import net.minecraft.client.resource.Resource;
+import net.minecraft.client.resource.language.TranslationStorage;
+import net.minecraft.client.resource.manager.ResourceManager;
+import net.minecraft.resource.Identifier;
 
 @Mixin(TranslationStorage.class)
 public abstract class TranslationStorageMixin {
 
-	@Shadow
-	Map<String, String> translations;
+	@Shadow 
+	private Map<String, String> translations;
 
-	@WrapOperation(
+	@Shadow
+	private void load(List<Resource> resources) throws IOException {
+	}
+
+	@Shadow
+	private void load(InputStream is) throws IOException {
+	}
+
+	@Inject(
+		method = "load(Lnet/minecraft/client/resource/manager/ResourceManager;Ljava/util/List;)V",
+		locals = LocalCapture.CAPTURE_FAILHARD,
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/util/List;)V"
+		)
+	)
+	private void osl$resource_loader$loadJsonFiles(ResourceManager manager, List<String> languageCodes, CallbackInfo ci, Iterator<String> it, String languageCode, String originalPath, String namespace) throws IOException {
+		String[] paths = new String[]{originalPath, String.format("lang/%s.json", languageCode),
+			String.format("lang/%s.json", languageCode.toLowerCase(Locale.ROOT)),
+			String.format("lang/%s.lang", languageCode.toLowerCase(Locale.ROOT))};
+		for (String s : paths){
+			load(manager.getResources(new Identifier(namespace, s)));
+		}
+	}
+
+	@Redirect(
 		method = "load(Lnet/minecraft/client/resource/manager/ResourceManager;Ljava/util/List;)V",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/util/List;)V"
 		)
 	)
-	private void osl$resource_loader$loadJsonFiles(TranslationStorage instance, List<Resource> resources, Operation<Void> original,
-												   @Local(ordinal = 0) String languageCode, @Local(ordinal = 1) String originalPath,
-												   @Local(ordinal = 2) String namespace, @Local(argsOnly = true) ResourceManager manager){
-		String[] paths = new String[]{originalPath, String.format("lang/%s.json", languageCode),
-			String.format("lang/%s.json", languageCode.toLowerCase(Locale.ROOT)),
-				String.format("lang/%s.lang", languageCode.toLowerCase(Locale.ROOT))};
-		for (String s : paths){
-			try {
-				original.call(instance, manager.getResources(new Identifier(namespace, s)));
-			} catch (IOException ignored) {
-			}
+	private void osl$resource_loader$noOpOriginalLoadCall(TranslationStorage instance, List<Resource> resources) {
+		// noop
+	}
+
+	@Inject(
+		method = "load(Ljava/util/List;)V",
+		locals = LocalCapture.CAPTURE_FAILHARD,
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/io/InputStream;)V"
+		)
+	)
+	private void osl$resource_loader$loadJsonFiles$2(List<Resource> resources, CallbackInfo ci, Iterator<Resource> it, Resource resource, InputStream is) throws IOException{
+		if (resource.getLocation().getPath().endsWith(".json")){
+			loadJson(is);
+		} else {
+			load(is); // load .lang
 		}
 	}
 
-	@WrapOperation(
+	@Redirect(
 		method = "load(Ljava/util/List;)V",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/io/InputStream;)V"
 		)
 	)
-	private void osl$resource_loader$loadJsonFiles$2(TranslationStorage instance, InputStream is, Operation<Void> original, @Local Resource resource){
-		if (resource.getLocation().getPath().endsWith(".json")){
-			loadJson(is);
-		} else {
-			original.call(instance, is);
-		}
+	private void osl$resource_loader$noOpOriginalLoadCall$2(TranslationStorage instance, InputStream is){
+		// noop
 	}
 
 	@Unique
--- libraries/resource-loader/resource-loader-mc17w43a-mc19w07a/src/main/java/net/ornithemc/osl/resource/loader/impl/mixin/client/TranslationStorageMixin.java
+++ libraries/resource-loader/resource-loader-mc17w43a-mc19w07a/src/main/java/net/ornithemc/osl/resource/loader/impl/mixin/client/TranslationStorageMixin.java
@@ -4,6 +4,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Locale;
 import java.util.Map;
@@ -13,10 +14,10 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
-
-import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
-import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
-import com.llamalad7.mixinextras.sugar.Local;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import net.minecraft.client.resource.language.TranslationStorage;
 import net.minecraft.client.resource.manager.ResourceManager;
@@ -30,37 +31,67 @@ public class TranslationStorageMixin {
 	@Shadow
 	private Map<String, String> translations;
 
-	@WrapOperation(
+	@Shadow
+	private void load(List<Resource> resources) throws IOException {
+	}
+
+	@Shadow
+	private void load(InputStream is) throws IOException {
+	}
+
+	@Inject(
+		method = "load(Lnet/minecraft/client/resource/manager/ResourceManager;Ljava/util/List;)V",
+		locals = LocalCapture.CAPTURE_FAILHARD,
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/util/List;)V"
+		)
+	)
+	private void osl$resource_loader$loadJsonFiles(ResourceManager manager, List<String> languageCodes, CallbackInfo ci, Iterator<String> it, String languageCode, String originalPath, String namespace) throws IOException{
+		String[] paths = new String[]{originalPath, String.format("lang/%s.lang", languageCode),
+			String.format("lang/%s.json", languageCode.toLowerCase(Locale.ROOT)),
+			String.format("lang/%s.lang", languageCode.toLowerCase(Locale.ROOT))};
+		for (String s : paths){
+			load(manager.getResources(new Identifier(namespace, s)));
+		}
+	}
+
+	@Redirect(
 		method = "load(Lnet/minecraft/client/resource/manager/ResourceManager;Ljava/util/List;)V",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/util/List;)V"
 		)
 	)
-	private void osl$resource_loader$loadJsonFiles(TranslationStorage instance, List<Resource> resources, Operation<Void> original,
-												   @Local(ordinal = 0) String languageCode, @Local(ordinal = 1) String originalPath,
-												   @Local(ordinal = 2) String namespace, @Local(argsOnly = true) ResourceManager manager) throws IOException{
-		String[] paths = new String[]{originalPath, String.format("lang/%s.lang", languageCode),
-			String.format("lang/%s.json", languageCode.toLowerCase(Locale.ROOT)),
-				String.format("lang/%s.lang", languageCode.toLowerCase(Locale.ROOT))};
-		for (String s : paths){
-			original.call(instance, manager.getResources(new Identifier(namespace, s)));
+	private void osl$resource_loader$noOpOriginalLoadCall(TranslationStorage instance, List<Resource> resources) {
+		// noop
+	}
+
+	@Inject(
+		method = "load(Ljava/util/List;)V",
+		locals = LocalCapture.CAPTURE_FAILHARD,
+		at = @At(
+			value = "INVOKE",
+			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/io/InputStream;)V"
+		)
+	)
+	private void osl$resource_loader$loadJsonFiles$2(List<Resource> resources, CallbackInfo ci, Iterator<Resource> it, Resource resource, InputStream is) throws IOException{
+		if (resource.getLocation().getPath().endsWith(".lang")){
+			loadLang(is);
+		} else {
+			load(is); // load .json
 		}
 	}
 
-	@WrapOperation(
+	@Redirect(
 		method = "load(Ljava/util/List;)V",
 		at = @At(
 			value = "INVOKE",
 			target = "Lnet/minecraft/client/resource/language/TranslationStorage;load(Ljava/io/InputStream;)V"
 		)
 	)
-	private void osl$resource_loader$loadJsonFiles$2(TranslationStorage instance, InputStream is, Operation<Void> original, @Local Resource resource) throws IOException {
-		if (resource.getLocation().getPath().endsWith(".lang")){
-			loadLang(is);
-		} else {
-			original.call(instance, is);
-		}
+	private void osl$resource_loader$noOpOriginalLoadCall$2(TranslationStorage instance, InputStream is){
+		// noop
 	}
 
 	@Unique
