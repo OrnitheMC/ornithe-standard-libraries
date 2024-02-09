62f9a5259c39e18a2d320e0aa8b7be8a84a64f37
Space Walker <spacedoesrs@gmail.com>
2024-02-09 16:40:06 +0100

    fix translations not loading in 1.5-, and fix injection failure

--- libraries/resource-loader/resource-loader-client-mca1.2.2-1624-mc11w48a/src/main/java/net/ornithemc/osl/resource/loader/impl/mixin/client/LanguageManagerMixin.java
+++ libraries/resource-loader/resource-loader-client-mca1.2.2-1624-mc11w48a/src/main/java/net/ornithemc/osl/resource/loader/impl/mixin/client/LanguageManagerMixin.java
@@ -4,15 +4,11 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.HashMap;
 import java.util.Locale;
-import java.util.Map;
 import java.util.Properties;
-import java.util.stream.Collectors;
 
 import org.quiltmc.parsers.json.JsonReader;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -28,64 +24,54 @@ import net.ornithemc.osl.resource.loader.impl.ResourceLoader;
 @Mixin(LanguageManager.class)
 public class LanguageManagerMixin {
 
-	@Shadow private Properties translations;
-
 	@Inject(
-		method = "<init>",
+		method = "loadTranslations(Ljava/util/Properties;Ljava/lang/String;)V",
 		at = @At(
 			value = "TAIL"
 		)
 	)
-	private void osl$resource_loader$loadModTranslations(CallbackInfo ci) {
-		String[] validFiles = new String[]{"/assets/%s/lang/%s.lang", "/assets/%s/lang/%s.json"};
-		String lang = "en_US";
+	private void osl$resource_loader$loadModTranslations(Properties translations, String lang, CallbackInfo ci) throws IOException {
 		for (ModTexturePack pack : ResourceLoader.getDefaultModResourcePacks()) {
 			ModMetadata mod = pack.getModMetadata();
+			String dir = String.format("/assets/%s/lang", mod.getId());
 
-			for (String path : validFiles){
-				if (path.endsWith(".json")){
-					addJsonFile(pack.getResource(String.format(path, mod.getId(), lang)));
-					addJsonFile(pack.getResource(String.format(path, mod.getId(), lang.toLowerCase(Locale.ROOT))));
-				} else if (path.endsWith(".lang")){
-					addLangFile(pack.getResource(String.format(path, mod.getId(), lang)));
-					addLangFile(pack.getResource(String.format(path, mod.getId(), lang.toLowerCase(Locale.ROOT))));
+			loadTranslationsJson(translations, pack.getResource(String.format("/%s.json", dir, lang)));
+			loadTranslationsJson(translations, pack.getResource(String.format("/%s.json", dir, lang.toLowerCase(Locale.ROOT))));
+			loadTranslationsLang(translations, pack.getResource(String.format("/%s.lang", dir, lang)));
+			loadTranslationsLang(translations, pack.getResource(String.format("/%s.lang", dir, lang.toLowerCase(Locale.ROOT))));
+		}
+	}
+
+	@Unique
+	private void loadTranslationsJson(Properties translations, InputStream is) throws IOException {
+		if (is != null){
+			try (JsonReader reader = JsonReader.json(new InputStreamReader(is))) {
+				reader.beginObject();
+				while (reader.hasNext()) {
+					translations.setProperty(reader.nextName(), reader.nextString());
+				}
+				reader.endObject();
+			}
+		}
+	}
+
+	@Unique
+	private void loadTranslationsLang(Properties translations, InputStream is) throws IOException {
+		if (is != null){
+			try (BufferedReader br = new BufferedReader(new InputStreamReader(is))) {
+				String line;
+				while ((line = br.readLine()) != null) {
+					line = line.trim();
+					if (line.isEmpty() || line.startsWith("#")) {
+						continue;
+					}
+					String[] args = line.split("=", 2);
+					if (args.length != 2) {
+						continue;
+					}
+					translations.setProperty(args[0], args[1]);
 				}
 			}
 		}
-	}
-
-	@Unique
-	private void addJsonFile(InputStream stream){
-		if (stream == null){
-			return;
-		}
-		JsonReader reader = JsonReader.json(new InputStreamReader(stream));
-
-		Map<String, String> entries = new HashMap<>();
-		try {
-			reader.beginObject();
-			while (reader.hasNext()) {
-				entries.put(reader.nextName(), reader.nextString());
-			}
-			reader.endObject();
-		} catch (IOException ignored) {
-		}
-		addLanguage(entries);
-	}
-
-	@Unique
-	private void addLangFile(InputStream stream){
-		if (stream == null){
-			return;
-		}
-		addLanguage(new BufferedReader(new InputStreamReader(stream)).lines()
-			.filter(line -> !line.startsWith("#"))
-			.map(line -> line.split("="))
-			.collect(Collectors.toMap(line -> line[0], line -> line[1])));
-	}
-
-	@Unique
-	private void addLanguage(Map<String, String> translations){
-		translations.forEach(this.translations::setProperty);
 	}
 }
