a4e4a9a1c01a72caf3c6637135791dbbfd92963d
Space Walker <spacedoesrs@gmail.com>
2026-02-10 20:29:19 +0100

    [networking] fix injection failure in 1.6.2+

--- libraries/networking-impl/networking-impl-mc12w21a-mc13w39b/src/main/java/net/ornithemc/osl/networking/impl/mixin/common/RemoteConnectionMixin.java
+++ libraries/networking-impl/networking-impl-mc12w21a-mc13w39b/src/main/java/net/ornithemc/osl/networking/impl/mixin/common/RemoteConnectionMixin.java
@@ -22,12 +22,28 @@ public class RemoteConnectionMixin {
 	@Inject(
 		method = "read",
 		cancellable = true,
+		require = 0, // will fail in 1.6.2+
 		at = @At(
 			value = "INVOKE",
 			target = "Ljava/util/List;add(Ljava/lang/Object;)Z"
 		)
 	)
-	private void osl$networking$asyncCustomPayloads(CallbackInfoReturnable<Boolean> cir, @Local Packet packet) {
+	private void osl$networking$asyncCustomPayloads1(CallbackInfoReturnable<Boolean> cir, @Local Packet packet) {
+		if (Connections.handleAsyncPacket(packet, listener)) {
+			cir.setReturnValue(true);
+		}
+	}
+
+	@Inject(
+		method = "read",
+		cancellable = true,
+		require = 0, // will fail in 1.6.1-
+		at = @At(
+			value = "INVOKE",
+			target = "Ljava/util/Queue;add(Ljava/lang/Object;)Z"
+		)
+	)
+	private void osl$networking$asyncCustomPayloads2(CallbackInfoReturnable<Boolean> cir, @Local Packet packet) {
 		if (Connections.handleAsyncPacket(packet, listener)) {
 			cir.setReturnValue(true);
 		}
