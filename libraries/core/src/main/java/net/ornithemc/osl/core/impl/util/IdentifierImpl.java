c88212c040ac1071d715600a5c3a3b17b349e3e1
Space Walker <spacedoesrs@gmail.com>
2026-04-19 12:12:50 +0200

    [core] NamespacedIdentifier transitive interface injection

---
+++ libraries/core/src/main/java/net/ornithemc/osl/core/impl/util/IdentifierImpl.java
@@ -0,0 +1,18 @@
+package net.ornithemc.osl.core.impl.util;
+
+import net.ornithemc.osl.core.api.util.NamespacedIdentifier;
+
+// This interface is used for transitive interface injection into Identifier.
+// Its purpose is to provide method implementations to keep the compiler happy.
+public interface IdentifierImpl extends NamespacedIdentifier {
+
+	@Override
+	default String namespace() {
+		throw new AbstractMethodError("Not implemented!");
+	}
+
+	@Override
+	default String identifier() {
+		throw new AbstractMethodError("Not implemented!");
+	}
+}
