b7a37131d1e597d2dde2ae08d13090b3f6375f20
Space Walker <spacedoesrs@gmail.com>
2026-07-09 12:30:40 +0200

    [networking] use mixin plugin for some mixins

--- libraries/networking-impl/networking-impl-mc12w21a-mc13w39b/src/main/java/net/ornithemc/osl/networking/impl/mixin/common/RemoteConnectionMixinOld.java
+++ libraries/networking-impl/networking-impl-mc12w21a-mc13w39b/src/main/java/net/ornithemc/osl/networking/impl/mixin/common/RemoteConnectionMixinOld.java
@@ -15,35 +15,19 @@ import net.minecraft.network.packet.Packet;
 import net.ornithemc.osl.networking.impl.Connections;
 
 @Mixin(RemoteConnection.class)
-public class RemoteConnectionMixin {
+public class RemoteConnectionMixinOld {
 
 	@Shadow private PacketHandler listener;
 
 	@Inject(
 		method = "read",
 		cancellable = true,
-		require = 0, // will fail in 1.6.2+
 		at = @At(
 			value = "INVOKE",
 			target = "Ljava/util/List;add(Ljava/lang/Object;)Z"
 		)
 	)
-	private void osl$networking$asyncCustomPayloads1(CallbackInfoReturnable<Boolean> cir, @Local Packet packet) {
-		if (Connections.handleAsyncPacket(packet, listener)) {
-			cir.setReturnValue(true);
-		}
-	}
-
-	@Inject(
-		method = "read",
-		cancellable = true,
-		require = 0, // will fail in 1.6.1-
-		at = @At(
-			value = "INVOKE",
-			target = "Ljava/util/Queue;add(Ljava/lang/Object;)Z"
-		)
-	)
-	private void osl$networking$asyncCustomPayloads2(CallbackInfoReturnable<Boolean> cir, @Local Packet packet) {
+	private void osl$networking$asyncCustomPayloads(CallbackInfoReturnable<Boolean> cir, @Local Packet packet) {
 		if (Connections.handleAsyncPacket(packet, listener)) {
 			cir.setReturnValue(true);
 		}
