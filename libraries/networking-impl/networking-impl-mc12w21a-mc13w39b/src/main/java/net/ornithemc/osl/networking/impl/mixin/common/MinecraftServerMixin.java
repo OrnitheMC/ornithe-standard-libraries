d0a71eafd35ab6b69d877ca73fdff2258747689e
Space Walker <spacedoesrs@gmail.com>
2026-07-11 16:13:50 +0200

    [networking] fix DISCONNECT not firing on server stop

---
+++ libraries/networking-impl/networking-impl-mc12w21a-mc13w39b/src/main/java/net/ornithemc/osl/networking/impl/mixin/common/MinecraftServerMixin.java
@@ -0,0 +1,42 @@
+package net.ornithemc.osl.networking.impl.mixin.common;
+
+import java.util.List;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.PlayerManager;
+import net.minecraft.server.entity.mob.player.ServerPlayerEntity;
+
+import net.ornithemc.osl.networking.api.server.ServerConnectionEvents;
+import net.ornithemc.osl.networking.impl.access.ServerNetworkHandlerAccess;
+import net.ornithemc.osl.networking.impl.server.ServerConnectionContext;
+
+@Mixin(MinecraftServer.class)
+public class MinecraftServerMixin {
+
+	@Shadow
+	private PlayerManager playerManager;
+
+	@Inject(
+		method = "stop",
+		at = @At(
+			value = "TAIL"
+		)
+	)
+	private void osl$networking$stop(CallbackInfo ci) {
+		@SuppressWarnings("unchecked")
+		List<ServerPlayerEntity> players = (List<ServerPlayerEntity>) this.playerManager.players;
+
+		for (ServerPlayerEntity player : players) {
+			ServerNetworkHandlerAccess networkHandler = (ServerNetworkHandlerAccess) player.networkHandler;
+			ServerConnectionContext connectionContext = networkHandler.osl$networking$connectionContext();
+
+			ServerConnectionEvents.DISCONNECT.invoker().accept(connectionContext);
+		}
+	}
+}
