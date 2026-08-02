5dc8d5ab530b0dd1b239b536f4ceb396f150c2d4
Space Walker <spacedoesrs@gmail.com>
2026-08-02 16:29:02 +0200

    [blocks] add BlockState registry fixer to Registry Sync

--- libraries/blocks/blocks-mca1.0.1_01-mc1.6.4/src/main/java/net/ornithemc/osl/blocks/impl/BlockRegistryImpl.java
+++ libraries/blocks/blocks-mca1.0.1_01-mc1.6.4/src/main/java/net/ornithemc/osl/blocks/impl/BlockRegistryImpl.java
@@ -6,6 +6,7 @@ import net.minecraft.block.Block;
 
 import net.ornithemc.osl.blocks.api.BlockEvents;
 import net.ornithemc.osl.blocks.impl.block.BlockIdFixer;
+import net.ornithemc.osl.blocks.impl.block.BlockStateRegistryFixer;
 import net.ornithemc.osl.core.api.util.NamespacedIdentifier;
 import net.ornithemc.osl.core.api.util.NamespacedIdentifiers;
 import net.ornithemc.osl.registries.api.registry.DefaultedRegistry;
@@ -88,6 +89,7 @@ public final class BlockRegistryImpl {
 	public static void init() {
 		SyncedRegistries.register(RegistryKeys.BLOCK);
 		SyncedRegistries.registerFixer(RegistryKeys.BLOCK, NamespacedIdentifiers.from("block/id"), new BlockIdFixer());
+		SyncedRegistries.registerFixer(RegistryKeys.BLOCK, NamespacedIdentifiers.from("blockstate"), new BlockStateRegistryFixer());
 	}
 
 	public static void unlock() {
