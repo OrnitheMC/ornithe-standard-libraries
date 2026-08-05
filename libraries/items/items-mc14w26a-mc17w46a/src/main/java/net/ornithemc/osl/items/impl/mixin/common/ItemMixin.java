47c2bf7b7fd6fe6677244cf91fe98664a9fca8d9
Space Walker <spacedoesrs@gmail.com>
2026-08-05 12:11:27 +0200

    [items] fix auto translation key assignment in 1.7-1.12

--- libraries/items/items-mc14w26a-mc17w46a/src/main/java/net/ornithemc/osl/items/impl/mixin/common/ItemMixin.java
+++ libraries/items/items-mc14w26a-mc17w46a/src/main/java/net/ornithemc/osl/items/impl/mixin/common/ItemMixin.java
@@ -2,6 +2,7 @@ package net.ornithemc.osl.items.impl.mixin.common;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -46,7 +47,22 @@ public class ItemMixin {
 			value = "HEAD"
 		)
 	)
-	private void osl$items$autoAssignTranslationKey(CallbackInfoReturnable<String> cir) {
+	private void osl$items$autoAssignTranslationKey1(CallbackInfoReturnable<String> cir) {
+		this.ensureTranslationKeyExists();
+	}
+
+	@Inject(
+		method = "getTranslationKey(Lnet/minecraft/item/ItemStack;)Ljava/lang/String;",
+		at = @At(
+			value = "HEAD"
+		)
+	)
+	private void osl$items$autoAssignTranslationKey2(CallbackInfoReturnable<String> cir) {
+		this.ensureTranslationKeyExists();
+	}
+
+	@Unique
+	private void ensureTranslationKeyExists() {
 		if (this.key == null) {
 			NamespacedIdentifier identifier = ItemRegistry.getIdentifier((Item) (Object) this);
 
