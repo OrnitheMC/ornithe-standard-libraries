2b7e7c28f1c91d5834e897b3af155b292f670b33
Space Walker <spacedoesrs@gmail.com>
2026-08-05 13:12:42 +0200

    [registries] fix bootstrap injection point

--- libraries/registries/registries-mc17w47a-mc18w31a/src/main/java/net/ornithemc/osl/registries/impl/mixin/common/BootstrapMixin.java
+++ libraries/registries/registries-mc17w47a-mc18w31a/src/main/java/net/ornithemc/osl/registries/impl/mixin/common/BootstrapMixin.java
@@ -1,9 +1,10 @@
 package net.ornithemc.osl.registries.impl.mixin.common;
 
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.Shadow;
+
+import com.llamalad7.mixinextras.injector.wrapmethod.WrapMethod;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
 
 import net.minecraft.Bootstrap;
 
@@ -13,15 +14,20 @@ import net.ornithemc.osl.registries.impl.registry.SyncedRegistriesImpl;
 @Mixin(Bootstrap.class)
 public class BootstrapMixin {
 
-	@Inject(
-		method = "init",
-		at = @At(
-			value = "INVOKE",
-			target = "Lnet/minecraft/stat/Stats;init()V"
-		)
-	)
-	private static void osl$registries$initAndLockRegistries(CallbackInfo ci) {
-		RegistriesImpl.init();
-		SyncedRegistriesImpl.init();
+	@Shadow
+	private static boolean initialized;
+
+	@WrapMethod(method = "init()V")
+	private static void osl$registries$initAndLockRegistries(Operation<Void> op) {
+		boolean wasInitialized = initialized;
+
+		// we could skip the call if initialized is already true
+		// but let's not mess with the bootstrap too much, eh
+		op.call();
+
+		if (!wasInitialized) {
+			RegistriesImpl.init();
+			SyncedRegistriesImpl.init();
+		}
 	}
 }
