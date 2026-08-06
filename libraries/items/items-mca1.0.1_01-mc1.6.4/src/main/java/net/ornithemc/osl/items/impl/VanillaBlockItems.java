aeb9d7802c4428be21355a88e6783b2a4ec57f3a
Space Walker <spacedoesrs@gmail.com>
2026-08-06 10:17:44 +0200

    [items] limit vanilla block item registration to items from Block fields

--- libraries/items/items-mca1.0.1_01-mc1.6.4/src/main/java/net/ornithemc/osl/items/impl/VanillaBlockItems.java
+++ libraries/items/items-mca1.0.1_01-mc1.6.4/src/main/java/net/ornithemc/osl/items/impl/VanillaBlockItems.java
@@ -1,33 +1,40 @@
 package net.ornithemc.osl.items.impl;
 
+import java.lang.reflect.Field;
+import java.lang.reflect.Modifier;
+
 import net.minecraft.block.Block;
 import net.minecraft.item.Item;
 
 import net.ornithemc.osl.blocks.api.block.Blocks;
 import net.ornithemc.osl.blocks.impl.BlockRegistryImpl;
+import net.ornithemc.osl.blocks.impl.VanillaBlocks;
 import net.ornithemc.osl.core.api.util.NamespacedIdentifier;
 import net.ornithemc.osl.items.impl.item.ItemUtil;
 
-final class VanillaBlockItems {
+public final class VanillaBlockItems {
+
+	public static final int MAX_ID = VanillaBlocks.MAX_ID;
 
 	static void init() {
-		for (int id = 0; id < VanillaItems.ITEM_ID_OFFSET; id++) {
-			Block block = Block.BY_ID[id];
-			Item item = Item.BY_ID[id];
+		for (Field f : Block.class.getDeclaredFields()) {
+			if (Modifier.isStatic(f.getModifiers()) && Block.class.isAssignableFrom(f.getType())) {
+				try {
+					Block block = (Block) f.get(null);
+					Item item = Item.BY_ID[block.id];
 
-			if (block != Blocks.AIR && item != null) {
-				NamespacedIdentifier identifier = BlockRegistryImpl.getIdentifier(block);
+					if (block != Blocks.AIR && item != null) {
+						NamespacedIdentifier identifier = BlockRegistryImpl.getIdentifier(block);
 
-				if (item.id != block.id) {
-					throw new IllegalStateException("item ID must match block ID for vanilla block items! (" + identifier + " has mismatched item ID " + item.id + " and block ID " + block.id + ")");
+						if (ItemRegistryImpl.getItem(identifier) != null) {
+							// some blocks have both a block item and special item form
+							identifier = identifier.suffixed("_block");
+						}
+
+						ItemRegistryImpl.register(identifier, block, item);
+					}
+				} catch (Throwable t) {
 				}
-
-				if (ItemRegistryImpl.getItem(identifier) != null) {
-					// some blocks have both a block item and special item form
-					identifier = identifier.suffixed("_block");
-				}
-
-				ItemRegistryImpl.register(identifier, block, item);
 			}
 		}
 
