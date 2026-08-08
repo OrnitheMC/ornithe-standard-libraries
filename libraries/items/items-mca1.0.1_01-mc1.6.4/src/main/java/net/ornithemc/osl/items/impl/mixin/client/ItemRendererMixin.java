0cb76472e549d775271e49ae4ded95837184b763
Space Walker <spacedoesrs@gmail.com>
2026-08-08 18:34:30 +0200

    [items] fix block item -> block conversion in ItemRenderer

---
+++ libraries/items/items-mca1.0.1_01-mc1.6.4/src/main/java/net/ornithemc/osl/items/impl/mixin/client/ItemRendererMixin.java
@@ -0,0 +1,36 @@
+package net.ornithemc.osl.items.impl.mixin.client;
+
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+
+import com.llamalad7.mixinextras.expression.Definition;
+import com.llamalad7.mixinextras.expression.Expression;
+import com.llamalad7.mixinextras.injector.wrapoperation.Operation;
+import com.llamalad7.mixinextras.injector.wrapoperation.WrapOperation;
+import com.llamalad7.mixinextras.sugar.Local;
+
+import net.minecraft.block.Block;
+import net.minecraft.client.render.entity.ItemRenderer;
+import net.minecraft.item.BlockItem;
+import net.minecraft.item.ItemStack;
+
+import net.ornithemc.osl.items.impl.mixin.common.BlockItemAccess;
+
+@Mixin(ItemRenderer.class)
+public class ItemRendererMixin {
+
+	@Definition(
+		id = "BY_ID",
+		field = "Lnet/minecraft/block/Block;BY_ID:[Lnet/minecraft/block/Block;"
+	)
+	@Expression("BY_ID[?]")
+	@WrapOperation(
+		method = "render",
+		at = @At(
+			value = "MIXINEXTRAS:EXPRESSION"
+		)
+	)
+	private Block osl$items$fixBlockCheck(Block[] BY_ID, int id, Operation<Block> op, @Local ItemStack item) {
+		return item.getItem() instanceof BlockItem ? op.call(BY_ID, ((BlockItemAccess) item.getItem()).accessBlock()) : null;
+	}
+}
