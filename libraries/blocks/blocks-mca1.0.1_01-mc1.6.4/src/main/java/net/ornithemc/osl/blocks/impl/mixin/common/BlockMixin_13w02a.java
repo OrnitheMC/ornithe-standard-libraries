e84ddeb06680a90f50dfee023daaeba9f45ecacc
Space Walker <spacedoesrs@gmail.com>
2026-09-05 13:14:26 +0200

    [blocks] fix invalid Mixin

--- libraries/blocks/blocks-mca1.0.1_01-mc1.6.4/src/main/java/net/ornithemc/osl/blocks/impl/mixin/common/BlockMixin_13w02a.java
+++ libraries/blocks/blocks-mca1.0.1_01-mc1.6.4/src/main/java/net/ornithemc/osl/blocks/impl/mixin/common/BlockMixin_13w02a.java
@@ -4,7 +4,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import net.minecraft.block.Block;
 
@@ -24,7 +24,7 @@ public class BlockMixin_13w02a {
 			value = "HEAD"
 		)
 	)
-	private void osl$blocks$autoAssignTranslationKey(CallbackInfo ci) {
+	private void osl$blocks$autoAssignTranslationKey(CallbackInfoReturnable<String> cir) {
 		if (this.key == null) {
 			NamespacedIdentifier identifier = BlockRegistry.getIdentifier((Block) (Object) this);
 
