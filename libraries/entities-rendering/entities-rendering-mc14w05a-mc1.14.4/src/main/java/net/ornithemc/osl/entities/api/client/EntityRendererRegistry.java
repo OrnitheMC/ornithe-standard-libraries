4c0dea5744169bc86d75b37929da891c0d63fffe
Space Walker <spacedoesrs@gmail.com>
2026-09-13 12:43:58 +0200

    [entities] add unsafe method to EntityRendererRegistry without generics
    This is necessary to make the API usable for MC versions that do not yet
    have the generics patches (Sparrow), for which the existing register
    method is unusable because of the compile time type safety checks

--- libraries/entities-rendering/entities-rendering-mc14w05a-mc1.14.4/src/main/java/net/ornithemc/osl/entities/api/client/EntityRendererRegistry.java
+++ libraries/entities-rendering/entities-rendering-mc14w05a-mc1.14.4/src/main/java/net/ornithemc/osl/entities/api/client/EntityRendererRegistry.java
@@ -21,4 +21,26 @@ public interface EntityRendererRegistry {
 	 */
 	<T extends Entity> void register(Class<T> type, Function<EntityRenderDispatcher, EntityRenderer<? super T>> factory);
 
+	/**
+	 * Registers the given entity renderer for the given entity type.
+	 * <p>
+	 * NOTE: this method only exists because generic type signatures are stripped
+	 * from all Minecraft jars from 1.8.2-pre4 and below, which makes using
+	 * {@linkplain #register(Class, Function)} impossible without special patches to
+	 * bring these generic type signatures back. Progress on this work is being
+	 * made, but has not yet been completed. In the meantime, this method exists to
+	 * allow entity renderers to be registered without compile time type safety
+	 * checks.
+	 * 
+	 * @param <T>     the entity type for which to register the renderer.
+	 * @param type    the entity type class.
+	 * @param factory the factory for the entity renderer to register.
+	 * 
+	 * @deprecated use {@linkplain #register(Class, Function)} if possible
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Deprecated
+	default <T extends Entity> void registerUnsafe(Class<T> type, Function<EntityRenderDispatcher, EntityRenderer> factory) {
+		this.register(type, (Function) factory);
+	}
 }
--- libraries/entities-rendering/entities-rendering-mca1.0.1_01-mc14w04b/src/main/java/net/ornithemc/osl/entities/api/client/EntityRendererRegistry.java
+++ libraries/entities-rendering/entities-rendering-mca1.0.1_01-mc14w04b/src/main/java/net/ornithemc/osl/entities/api/client/EntityRendererRegistry.java
@@ -21,4 +21,26 @@ public interface EntityRendererRegistry {
 	 */
 	<T extends Entity> void register(Class<T> type, Supplier<EntityRenderer<? super T>> factory);
 
+	/**
+	 * Registers the given entity renderer for the given entity type.
+	 * <p>
+	 * NOTE: this method only exists because generic type signatures are stripped
+	 * from all Minecraft jars from 1.8.2-pre4 and below, which makes using
+	 * {@linkplain #register(Class, Supplier)} impossible without special patches to
+	 * bring these generic type signatures back. Progress on this work is being
+	 * made, but has not yet been completed. In the meantime, this method exists to
+	 * allow entity renderers to be registered without compile time type safety
+	 * checks.
+	 * 
+	 * @param <T>     the entity type for which to register the renderer.
+	 * @param type    the entity type class.
+	 * @param factory the factory for the entity renderer to register.
+	 * 
+	 * @deprecated use {@linkplain #register(Class, Supplier)} if possible
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Deprecated
+	default <T extends Entity> void registerUnsafe(Class<T> type, Supplier<EntityRenderer> factory) {
+		this.register(type, (Supplier) factory);
+	}
 }
