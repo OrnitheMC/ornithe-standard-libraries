df03bb3077f8f344c98d59311f16f8005e116c62
Space Walker <spacedoesrs@gmail.com>
2026-09-13 12:37:54 +0200

    [blockentities] add unsafe method to BlockEntityRendererRegistry without generics
    This is necessary to make the API usable for MC versions that do not yet
    have the generics patches (Sparrow), for which the existing register
    method is unusable because of the compile time type safety checks

--- libraries/blockentities-rendering/blockentities-rendering-mca1.0.1_01-mc1.14.4/src/main/java/net/ornithemc/osl/blockentities/api/client/BlockEntityRendererRegistry.java
+++ libraries/blockentities-rendering/blockentities-rendering-mca1.0.1_01-mc1.14.4/src/main/java/net/ornithemc/osl/blockentities/api/client/BlockEntityRendererRegistry.java
@@ -21,4 +21,26 @@ public interface BlockEntityRendererRegistry {
 	 */
 	<T extends BlockEntity> void register(Class<T> type, Supplier<BlockEntityRenderer<? super T>> factory);
 
+	/**
+	 * Registers the given block entity renderer for the given block entity type.
+	 * <p>
+	 * NOTE: this method only exists because generic type signatures are stripped
+	 * from all Minecraft jars from 1.8.2-pre4 and below, which makes using
+	 * {@linkplain #register(Class, Supplier)} impossible without special patches to
+	 * bring these generic type signatures back. Progress on this work is being
+	 * made, but has not yet been completed. In the meantime, this method exists to
+	 * allow block entity renderers to be registered without compile time type
+	 * safety checks.
+	 * 
+	 * @param <T>     the block entity type for which to register the renderer.
+	 * @param type    the block entity type class.
+	 * @param factory the factory for the block entity renderer to register.
+	 * 
+	 * @deprecated use {@linkplain #register(Class, Supplier)} if possible
+	 */
+	@SuppressWarnings({ "unchecked", "rawtypes" })
+	@Deprecated
+	default <T extends BlockEntity> void registerUnsafe(Class<T> type, Supplier<BlockEntityRenderer> factory) {
+		this.register(type, (Supplier) factory);
+	}
 }
